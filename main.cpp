cfa209c359a3219598f5a63eb8d0e56a9615eeaf
septa97 <jancharles.adona@gmail.com>
2020-03-23 10:16:41 +0800

    Refactor and use a proper refresh rate (480 Hz)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #define KEYPAD_SIZE 16
 #define WIDTH 64
 #define HEIGHT 32
+#define REFRESH_RATE 480
 
 #include "chip8.cpp"
 
@@ -11,26 +12,26 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 SDL_Keycode keymap[KEYPAD_SIZE] = {
-    SDLK_x,
-    SDLK_1,
-    SDLK_2,
-    SDLK_3,
-    SDLK_q,
-    SDLK_w,
-    SDLK_e,
-    SDLK_a,
-    SDLK_s,
-    SDLK_d,
-    SDLK_z,
-    SDLK_c,
-    SDLK_4,
-    SDLK_r,
-    SDLK_f,
-    SDLK_v
+    SDLK_1, // 1
+    SDLK_2, // 2
+    SDLK_3, // 3
+    SDLK_4, // C
+    SDLK_q, // 4
+    SDLK_w, // 5
+    SDLK_e, // 6
+    SDLK_r, // D
+    SDLK_a, // 7
+    SDLK_s, // 8
+    SDLK_d, // 9
+    SDLK_c, // E
+    SDLK_z, // A
+    SDLK_x, // 0
+    SDLK_c, // B
+    SDLK_v	// F
 };
 
 void draw_frame(SDL_Texture* texture, SDL_Renderer* renderer, uint32_t* pixels) {
-    SDL_UpdateTexture(texture, NULL, pixels, 64 * sizeof(uint32_t));
+    SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(uint32_t));
     SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);
 }
@@ -41,8 +42,7 @@ int main(int argc, char *argv[]) {
     SDL_Texture* texture = NULL;
     SDL_Event e;
 
-    uint32_t pixels[64 * 32];
-    // bool pixels[64 * 32];
+    uint32_t pixels[WIDTH * HEIGHT];
 
     Chip8 chip8 = Chip8();
 
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
 
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    // SDL_SetWindowSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_SetWindowSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 64, 32); // Use #define on this
 
     if (texture == NULL) {
@@ -81,8 +81,19 @@ int main(int argc, char *argv[]) {
         
     bool quit = false;
 
+    std::chrono::duration<double> tick(1.0/REFRESH_RATE);
+
     while (!quit) {
+        auto start = std::chrono::system_clock::now();
+
         chip8.emulate_cycle();
+
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> exec_time = end - start;
+
+        if (exec_time.count() < 1.0/REFRESH_RATE) {
+            std::this_thread::sleep_for(tick - exec_time);
+        }
 
         while (SDL_PollEvent(&e)) { // 1 if there's an event, 0 if none
             // user requests to quit
@@ -108,7 +119,7 @@ int main(int argc, char *argv[]) {
             chip8.drawFlag = false;
 
             // Store CHIP-8 gfx to pixels
-            for (int i = 0; i < 64 * 32; i++) {
+            for (int i = 0; i < WIDTH * HEIGHT; i++) {
                 uint8_t pixel = chip8.gfx[i];
                 // pixel values are either 1 or 0, multiply it by 0x00FFFFFF (which represents RGB) for either black or white
                 // then bit mask with (using OR) on 0xFF000000
@@ -121,9 +132,6 @@ int main(int argc, char *argv[]) {
 
             draw_frame(texture, renderer, pixels);
         }
-
-        // TODO: research on emulator speed
-        // std::this_thread::sleep_for(std::chrono::microseconds(1200));
     }
 
     SDL_DestroyWindow(window);
